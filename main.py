b037deec8b22108ae48e5f41ab07bcf80be3e5d6
Levi_Stoops <sto.levi@gmail.com>
2026-08-03 14:32:26 +0200

    Delta time added

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,7 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000.0  # Limit to 60 FPS and get delta time in seconds
 
 if __name__ == "__main__":
     main()
