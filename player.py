a20d643408b17cc72f43e25dfa760d5878370f6d
Levi_Stoops <sto.levi@gmail.com>
2026-08-05 11:26:31 +0200

    added move method and modified update method in player.py

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y) -> None:
@@ -22,6 +22,12 @@ class Player(CircleShape):
     def rotate(self, dt) -> None:
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt: float) -> None:
+            unit_vector = pygame.Vector2(0, 1)
+            rotated_vector = unit_vector.rotate(self.rotation)
+            rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+            self.position += rotated_with_speed_vector
+
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
@@ -29,4 +35,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    
             
