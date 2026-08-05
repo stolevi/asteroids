3f96d2da92dc57f3c2e737ea0ddd9aa3b6edff47
Levi_Stoops <sto.levi@gmail.com>
2026-08-05 10:44:13 +0200

    added player

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state 
-
+from player import Player
 
 
 def main():
@@ -12,14 +12,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(locals())
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        screen.fill("black")        
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0  # Limit to 60 FPS and get delta time in seconds
-
+        
 if __name__ == "__main__":
     main()
