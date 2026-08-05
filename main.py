8a721500831acab9f88bd2b9d0ff9a4bf80a57e7
Levi_Stoops <sto.levi@gmail.com>
2026-08-05 11:40:44 +0200

    created groups updatable & drawable in main.py

--- main.py
+++ main.py
@@ -12,15 +12,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")        
-        player.draw(screen)
-        player.update(dt)
+        screen.fill("black")
+        updatable.update(dt)        
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0  # Limit to 60 FPS and get delta time in seconds
         
